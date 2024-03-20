0013c41063c85389d850fba34e41af5f40c7fc55
다히 <102225999+daheeya@users.noreply.github.com>
2024-03-20 13:08:12 +0900

    model 코딩, view 및 template 코딩

---
+++ ch3/polls/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+#추가
+from polls.models import Question
+
+# Create your views here.
+def index(request):
+    latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_question_list':latest_question_list}
+    return render(request, 'polls/index.html',context)
